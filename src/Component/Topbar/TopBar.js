0d617ab688bd74fa3ba94193a31d7efa1835e781
Swayam Gupta <77153230+swayamg20@users.noreply.github.com>
2024-05-23 16:18:10 +0530

    mobile responsiveness

--- src/Component/Topbar/TopBar.js
+++ src/Component/Topbar/TopBar.js
@@ -8,11 +8,16 @@ import {
   InputLabel,
   FormControl,
   Box,
+  useMediaQuery,
+  ThemeProvider,
+  createTheme,
 } from "@mui/material";
 
 const TopBar = () => {
   const dispatch = useDispatch();
   const { filters } = useSelector((state) => state.jobState);
+  const theme = createTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
 
   const handleMinBasePayChange = (e) => {
     dispatch(setFilter({ minBasePay: Number(e.target.value) }));
@@ -39,76 +44,85 @@ const TopBar = () => {
   };
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <FormControl fullWidth margin="normal">
-        <InputLabel>Min Base Pay</InputLabel>
-        <Select
-          value={filters.minBasePay || ""}
-          onChange={handleMinBasePayChange}
-          label="Min Base Pay"
-        >
-          <MenuItem value="">Select</MenuItem>
-          {[...Array(11).keys()].map((i) => (
-            <MenuItem key={i} value={i * 10}>
-              {i * 10} LPA
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: isMobile ? "column" : "row",
+          margin: "10px",
+          maxWidth: "80vw",
+        }}
+      >
+        <FormControl margin="normal">
+          <InputLabel>Min Base Pay</InputLabel>
+          <Select
+            value={filters.minBasePay || ""}
+            onChange={handleMinBasePayChange}
+            label="Min Base Pay"
+            sx={{ minWidth: "10vw" }}
+          >
+            <MenuItem value="">Select</MenuItem>
+            {[...Array(11).keys()].map((i) => (
+              <MenuItem key={i} value={i * 10}>
+                {i * 10} LPA
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-      <FormControl fullWidth margin="normal">
-        <TextField
-          label="Min Experience"
-          type="number"
-          value={filters.minExperience || ""}
-          onChange={handleMinExperienceChange}
-          fullWidth
-        />
-      </FormControl>
+        <FormControl margin="normal">
+          <TextField
+            label="Min Experience"
+            type="number"
+            value={filters.minExperience || ""}
+            onChange={handleMinExperienceChange}
+          />
+        </FormControl>
 
-      <FormControl fullWidth margin="normal">
-        <TextField
-          label="Company Name"
-          type="text"
-          value={filters.companyName}
-          onChange={handleCompanyNameChange}
-          fullWidth
-        />
-      </FormControl>
+        <FormControl margin="normal">
+          <TextField
+            label="Company Name"
+            type="text"
+            value={filters.companyName}
+            onChange={handleCompanyNameChange}
+          />
+        </FormControl>
 
-      <FormControl fullWidth margin="normal">
-        <TextField
-          label="Location"
-          type="text"
-          value={filters.location}
-          onChange={handleLocationChange}
-          fullWidth
-        />
-      </FormControl>
+        <FormControl margin="normal">
+          <TextField
+            label="Location"
+            type="text"
+            value={filters.location}
+            onChange={handleLocationChange}
+            fullWidth
+          />
+        </FormControl>
 
-      <FormControl fullWidth margin="normal">
-        <InputLabel>Remote/Onsite</InputLabel>
-        <Select
-          value={filters.remoteOnsite}
-          onChange={handleRemoteOnsiteChange}
-          label="Remote/Onsite"
-        >
-          <MenuItem value="">Select</MenuItem>
-          <MenuItem value="remote">Remote</MenuItem>
-          <MenuItem value="onsite">Onsite</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl margin="normal">
+          <InputLabel>Remote/Onsite</InputLabel>
+          <Select
+            value={filters.remoteOnsite}
+            onChange={handleRemoteOnsiteChange}
+            label="Remote/Onsite"
+            sx={{ minWidth: "10vw" }}
+          >
+            <MenuItem value="">Select</MenuItem>
+            <MenuItem value="remote">Remote</MenuItem>
+            <MenuItem value="onsite">Onsite</MenuItem>
+          </Select>
+        </FormControl>
 
-      <FormControl fullWidth margin="normal">
-        <TextField
-          label="Role"
-          type="text"
-          value={filters.role}
-          onChange={handleRoleChange}
-          fullWidth
-        />
-      </FormControl>
-    </Box>
+        <FormControl margin="normal">
+          <TextField
+            label="Role"
+            type="text"
+            value={filters.role}
+            onChange={handleRoleChange}
+            fullWidth
+          />
+        </FormControl>
+      </Box>
+    </ThemeProvider>
   );
 };
 
